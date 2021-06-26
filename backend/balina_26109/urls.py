2957c7b47f11a92907e59aafa6ea01b429f3d897
Crowdbotics <team@crowdbotics.com>
2021-06-26 04:31:03 +0000

    Update admin title

--- backend/balina_26109/urls.py
+++ backend/balina_26109/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Balina"
-admin.site.site_title = "Balina Admin Portal"
-admin.site.index_title = "Balina Admin"
+admin.site.site_header = "HandBallin"
+admin.site.site_title = "HandBallin Admin Portal"
+admin.site.index_title = "HandBallin Admin"
 
 # swagger
 api_info = openapi.Info(
